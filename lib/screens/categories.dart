c0366278f9656a000f06814b8d1aa42bcd2caeed
Ogundare Oluwafemi <ogundarearnold@gmail.com>
2023-10-27 14:27:49 +0100

    used the .where and the .contains

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,19 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:meals/data/dummy_data.dart';
+import 'package:meals/models/category.dart';
 import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
-  void _selectCategory(BuildContext context) {
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where(
+          (meal) => meal.categories.contains(category.id),
+        )
+        .toList();
+
     Navigator.push(
       context,
       MaterialPageRoute(
         builder: (context) {
-          return const MealsScreen(
-            title: 'Some title',
-            meals: [],
+          return MealsScreen(
+            title: category.title,
+            meals: filteredMeals,
           );
         },
       ),
@@ -39,7 +46,7 @@ class CategoriesScreen extends StatelessWidget {
             CategoryGridItem(
               category: category,
               onSelectCategory: () {
-                _selectCategory(context);
+                _selectCategory(context, category);
               },
             )
         ],
