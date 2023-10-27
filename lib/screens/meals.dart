37cc4e3d4fc78d6775d9455c80074f195c400c62
Ogundare Oluwafemi <ogundarearnold@gmail.com>
2023-10-27 15:49:54 +0100

    Added the meals in list on the other screen away from the Menu Items

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({
@@ -16,7 +17,9 @@ class MealsScreen extends StatelessWidget {
     Widget content = ListView.builder(
       itemCount: meals.length,
       itemBuilder: (context, index) {
-        return Text(meals[index].title);
+        return MealItem(
+          meal: meals[index],
+        );
       },
     );
     if (meals.isEmpty) {
