93f9c0fb2afbc6bc58bbf5639c64a5a2e38888fa
Ogundare Oluwafemi <ogundarearnold@gmail.com>
2023-11-03 13:08:40 +0100

    Added the Filter Screen from the Tabs page through the Drawer

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
 import 'package:meals/screens/categories.dart';
+import 'package:meals/screens/filters.dart';
 import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/main_drawer.dart';
 
@@ -51,6 +52,13 @@ class _TabScreenState extends State<TabScreen> {
 
   void _setScreen(String identifier) {
     if (identifier == 'filters') {
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) {
+            return const FiltersScreen();
+          },
+        ),
+      );
     } else {
       Navigator.of(context).pop();
     }
